91a2d3772b49f010363e3359e7efabedb43caade
Sergio Gutierrez <sergiogutierrez0611@gmail.com>
2020-11-04 20:22:50 -0400

    Project finished, bugs fixed

--- js/app.js
+++ js/app.js
@@ -1,5 +1,8 @@
 const $inner = document.querySelector('.content__inner');
 
+const $span = document.querySelector('.content__inner__error'),
+    $input = document.querySelector('.content__inner__form-input');
+
 const media = window.matchMedia('(max-width: 1023px)');
 
 const mediaQueries = (e) => {
@@ -9,6 +12,19 @@ const mediaQueries = (e) => {
     }
         console.log('What the fuck did you do???');
 }
+
+function input() {
+    
+    if ($input.validity.valid) {
+        $span.classList.add('none')
+        console.log('You get it');
+    } else {
+        console.log('Nah, u screwed it up');
+        $span.classList.remove('none')
+    }
+}
+
+document.addEventListener('keyup', input);
 
 media.addEventListener('change', mediaQueries);
 
